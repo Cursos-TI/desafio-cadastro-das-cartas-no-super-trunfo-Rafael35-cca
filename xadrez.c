8dc3b1cb1100e17e43d89b0457c8978e3f13cf8b
Rafael35-cca <rafael1henrique45@gmail.com>
2025-03-22 21:19:18 +0000

    nivel novato trabalho

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
 int main(){
-    printf("desafio xadrez !/n");
+    char carta [1] = 1;
+    char estado = 'R';
+    char codigo [10] = "R02";
+    char cidade [20] = "Rio de Janeiro";
+    int populaçao = 25.000;
+    float área = 5070.47;
+    float PIB = 800.000;
+    int turisticos = 10;
 
-}
+
+    char carta [2] = 2;
+    char estado = 'S';
+    char codigo [10] = "S01";
+    char cidade [20] = "Santa catarina";
+    int populaçao = 10.000;
+    float área = 2040.00;
+    float PIB = 600.000;
+    int turisticos = 10;
+    return 0;
+    
+    
+
+
+
+
+
+}    
+    
+
