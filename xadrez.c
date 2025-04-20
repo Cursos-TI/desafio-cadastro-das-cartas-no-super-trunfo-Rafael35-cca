526afa653c65da291bd373db6e1d6ae72ed954aa
Rafael35-cca <rafael1henrique45@gmail.com>
2025-04-20 05:49:09 +0000

    nivel aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,24 +1,53 @@
 #include <stdio.h> 
 
 int main(){
-    int carta = 1;
+    char carta = 1;
     char estado = 'R';
     char codigo [10] = "R02";
     char cidade [20] = "Rio de Janeiro";
-    int populaçao = 25.000;
+    int populacao = 25.000;
     float área = 5070.47;
     float PIB = 800.000;
     int turisticos = 10;
+    float densidade = 62867;
+ float Percapita =50000
+
+    char carta = 2;
+     char estado2 = 'S';
+     char codigo2 [10] = "S01";
+     char cidade2[20] = "Santa catarina";
+     int populaçao2 = 10.000;
+     float área2 = 2040.00;
+     float PIB2 = 600.000;
+     int turisticos2 = 10;
+     float densidade = 80000;
+     float Percapita = 60000;
+
+    float densidade,area2;
+     float media;
+ 
+     printf("digite sua primeira densidade populacional1: \n");
+     scanf("%f", densidade);
+ 
+     printf("digite sua segunda area2: \n");
+     scanf("%f", area2);
+ 
+     media = (densidade + area2) / 2;
 
 
-    int carta = 2;
-    char estado = 'S';
-    char codigo [10] = "S01";
-    char cidade [20] = "Santa catarina";
-    int populaçao = 10.000;
-    float área = 2040.00;
-    float PIB = 600.000;
-    int turisticos = 10;
+     float capita,populacao1;
+   float media1;
+
+   printf("digite sua primeira percapita: \n");
+      scanf("%f", Percapita);
+
+      printf("digite sua segunda popuçacao1: \n");
+      scanf("%f", populacao1);
+
+ 
+
+
+
     return 0;
     
     
